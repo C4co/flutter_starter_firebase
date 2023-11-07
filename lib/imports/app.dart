7caae09c91e8b32de660898a4bb3145fa895aa20
Carlos Costa <carllos.nc@gmail.com>
2023-11-06 23:57:35 -0300

    Remove unused import statement.

--- lib/imports/app.dart
+++ lib/imports/app.dart
@@ -1,6 +1,5 @@
 export '/core/core.dart';
 export '/components/components.dart';
 export '/pages/pages.dart';
-export '/data/data.dart';
 export '/notifiers/notifiers.dart';
 export '/services/services.dart';
